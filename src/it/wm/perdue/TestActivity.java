dc0e5dba41c6116889532a55aa83b610bc8ab1a2
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-10-22 23:37:58 +0200

    [TestActivity] Aggiunto parsing del json scaricato da wm.HTTPAccess

--- src/it/wm/perdue/TestActivity.java
+++ src/it/wm/perdue/TestActivity.java
@@ -1,5 +1,14 @@
 package it.wm.perdue;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 import it.wm.HTTPAccess;
 //import it.wm.JSONDecoder;
 import android.app.Activity;
@@ -24,7 +33,7 @@ public class TestActivity extends Activity {
         	public void onHTTPResponseReceived(String response) {
         		((android.widget.TextView)TestActivity.this
         				.findViewById(R.id.textView1)).setText(response);
-        		//JSONDecoder.decode(response);
+        		parseJSON(response);
         	}
         	public void onHTTPerror() {
         		((android.widget.TextView)TestActivity.this
@@ -34,6 +43,74 @@ public class TestActivity extends Activity {
         });
         
 
+	}
+	
+	private void parseJSON(String jsonStr) {
+		JSONObject obj = null;
+		try {
+			obj = new JSONObject(jsonStr);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JSONArray arr = null;
+		try {
+			arr = obj.getJSONArray("Esercente");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayList<CommentDataModel> dataModel = new ArrayList<CommentDataModel>(arr.length());
+		for (int i=0; i<arr.length(); i++) {
+			Gson gson = new Gson();
+			CommentDataModel data = null;
+			try {
+				data = gson.fromJson(arr.getJSONObject(i).toString(), CommentDataModel.class);
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			dataModel.add(data);
+		}
+		
+		android.widget.TextView tv = ((android.widget.TextView)findViewById(R.id.textView1));
+		StringBuilder b = new StringBuilder(tv.getText());
+		
+		for (int i=0; i<dataModel.size(); i++) {
+			CommentDataModel data = dataModel.get(i);
+			b.append("\n****************\n");
+			b.append(data.comment_author);
+			b.append("\n===\n");
+			b.append(data.comment_content);
+			b.append("\n===\n");
+			b.append(data.comment_date);
+			b.append("\n===\n");
+			b.append(""+data.comment_ID);	
+		}
+		tv.setText(b.toString());
+	}
+	
+	
+	private class CommentDataModel {
+		private String comment_author;
+		private String comment_content;
+		private String comment_date;
+		private int comment_ID;
+		
+		public void   setComment_author(String s) {comment_author = s;}
+		public String comment_author() {return comment_author;}
+		
+		public void   setComment_content(String s) {comment_content = s;}
+		public String comment_content() {return comment_content;}
+		
+		public void   setComment_date(String s) {comment_date = s;}
+		public String comment_date() {return comment_date;}
+		
+		public void setComment_ID(int id) {comment_ID = id;}
+		public int  comment_ID() {return comment_ID;}	
 	}
 
 }
