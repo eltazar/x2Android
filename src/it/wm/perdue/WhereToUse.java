3be8cb9679106a5fb65ea49fb148f82530e724d4
Mario Greco <mrgreco3@gmail.com>
2012-10-24 12:56:24 +0200

    [WherToUse]: ora viene usata la sherlock action bar

--- src/it/wm/perdue/WhereToUse.java
+++ src/it/wm/perdue/WhereToUse.java
@@ -1,27 +1,29 @@
 package it.wm.perdue;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
 import it.wm.perdue.R;
 import android.os.Bundle;
-import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
-public class WhereToUse extends Activity {
+public class WhereToUse extends SherlockActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_where_to_use);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_where_to_use, menu);
-        getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#d65151")));
+        getSupportMenuInflater().inflate(R.menu.activity_where_to_use, menu);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#d65151")));
         return true;
     }
 
