c212ade7a778d858688287766b5064dd81484540
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-10-28 23:35:04 +0100

    [it.wm.perdue.businessLogic] Aggiunte classi per logica di business, con supporto preliminare e altamente instabile e inaffidabile alla [de]serializzazione JSON.

---
+++ src/it/wm/perdue/businessLogic/Coupon.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+
+package it.wm.perdue.businessLogic;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.net.URL;
+import java.util.Date;
+
+/**
+ * @author Gabriele "Whisky" Visconti
+ */
+public class Coupon {
+    @SerializedName("idofferta")
+    private int     id                 = -1;
+    @SerializedName("offerta_title")
+    private String  titolo             = null;
+    @SerializedName("offerta_titolo_breve")
+    private String  titoloBreve        = null;
+    @SerializedName("offerta_foto_big")
+    private URL     urlImmagine        = null;
+    @SerializedName("coupon_valore_acquisto")
+    private float   valoreAcquisto     = 0;
+    @SerializedName("coupon_valore_facciale")
+    private float   valoreFacciale     = 0;
+    @SerializedName("offerta_sconto_va")
+    private float   sconto             = 0;
+    @SerializedName("offerta_sconto_per")
+    private String  scontoPer          = null; // ????????
+    @SerializedName("offerta_descrizione_estesa")
+    private String  descrizione        = null;
+    @SerializedName("offerta_descrizione_breve")
+    private String  descrizioneBreve   = null;
+    @SerializedName("offerta_condizioni_sintetiche")
+    private String  condizioni         = null;
+    @SerializedName("coupon_periodo_dal")
+    private Date    couponPeriodoDal   = null; // ?????????
+    @SerializedName("offerta_pediodo_dal")
+    private Date    inizioValidita     = null;
+    @SerializedName("offerta_periodo_al")
+    private Date    fineValidita       = null;
+    @SerializedName("idesercente")
+    private int     idEsercente        = -1;
+    @SerializedName("esercente_nome")
+    private String  nomeEsercente      = null;
+    @SerializedName("esercente_indirizzo")
+    private String  indirizzoEsercente = null;
+    @SerializedName("esercente_comune")
+    private boolean esercenteComune    = false;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getTitoloBreve() {
+        return titoloBreve;
+    }
+
+    public URL getUrlImmagine() {
+        return urlImmagine;
+    }
+
+    public float getValoreAcquisto() {
+        return valoreAcquisto;
+    }
+
+    public float getValoreFacciale() {
+        return valoreFacciale;
+    }
+
+    public float getSconto() {
+        return sconto;
+    }
+
+    public String getScontoPer() {
+        return scontoPer;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public String getDescrizioneBreve() {
+        return descrizioneBreve;
+    }
+
+    public String getCondizioni() {
+        return condizioni;
+    }
+
+    public Date getCouponPeriodoDal() {
+        return couponPeriodoDal;
+    }
+
+    public Date getInizioValidita() {
+        return inizioValidita;
+    }
+
+    public Date getFineValidita() {
+        return fineValidita;
+    }
+
+    public int getIdEsercente() {
+        return idEsercente;
+    }
+
+    public String getNomeEsercente() {
+        return nomeEsercente;
+    }
+
+    public String getIndirizzoEsercente() {
+        return indirizzoEsercente;
+    }
+
+    public boolean isEsercenteComune() {
+        return esercenteComune;
+    }
+
+}
---
+++ src/it/wm/perdue/businessLogic/Esercente.java
@@ -0,0 +1,102 @@
+
+package it.wm.perdue.businessLogic;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Gabriele "Whisky" Visconti
+ */
+public class Esercente {
+    @SerializedName("IDEsercente")
+    private int     id             = -1;
+    @SerializedName("Insegna_Esercente")
+    private String  insegna        = null;
+    @SerializedName("Indirizzo_Esercente")
+    private String  indirizzo      = null;
+    @SerializedName("Citta_Esercente")
+    private String  citta          = null;
+    @SerializedName("Zona_Esercente")
+    private String  zona           = null;
+    @SerializedName("Telefono_Esercente")
+    private String  telefono       = null;
+    @SerializedName("Email_Esercente")
+    private String  email          = null;
+    @SerializedName("Url_Esercente")
+    private String  url            = null; // TODO: cambiare classe in URL
+    @SerializedName("Giorno_chiusura_Esercente")
+    private String  giornoChiusura = null;
+    @SerializedName("Tipo_Teser")
+    private String  tipologia      = null;
+    @SerializedName("Note_Varie_CE")
+    private String  noteVarie      = null; // TODO: riguardarsi cosa contiene e
+                                            // cambiare nome.
+    @SerializedName("Ulteriori_Informazioni")
+    private boolean ulterioriInfo  = false;
+    @SerializedName("Esercente_Virtuale")
+    private boolean virtuale       = false;
+    @SerializedName("Latitudine")
+    private double  latitude       = 0.0;
+    @SerializedName("Longitudine")
+    private double  longitude      = 0.0;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getInsegna() {
+        return insegna;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getGiornoChiusura() {
+        return giornoChiusura;
+    }
+
+    public String getTipologia() {
+        return tipologia;
+    }
+
+    public String getNoteVarie() {
+        return noteVarie;
+    }
+
+    public boolean isUlterioriInfo() {
+        return ulterioriInfo;
+    }
+
+    public boolean isVirtuale() {
+        return virtuale;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+}
