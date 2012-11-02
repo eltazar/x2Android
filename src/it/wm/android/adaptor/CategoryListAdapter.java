961437745da5be5f90669c8de991969018fdef50
Mario Greco <mrgreco3@gmail.com>
2012-11-02 16:19:14 +0100

    [it.wm.android.adaptor]: inserite classi per la gestione dell'arrayAdaptor per le macro categorie di "dove usarla" e i relativi layouts per righe e listView

---
+++ src/it/wm/android/adaptor/CategoryListAdapter.java
@@ -0,0 +1,80 @@
+
+package it.wm.android.adaptor;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import it.wm.perdue.R;
+
+public class CategoryListAdapter extends BaseAdapter {
+
+    private LayoutInflater _inflater;
+    private String[]       _items;
+    private Context        _context;
+    private Integer[]      _images;
+
+    public CategoryListAdapter(Context context, String[] categories, Integer[] images) {
+        _inflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        _items = categories;
+        _context = context;
+        _images = images;
+    }
+
+    @Override
+    public int getCount() {
+        return _items.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return _items[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position + 1;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+        if (v == null) {
+            v = ((LayoutInflater) _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                    .inflate(R.layout.category_row, null);
+        }
+
+        // String o = (String)_items.getItem(position);
+        // if (o != null) {
+        // TextView tt = (TextView) v.findViewById(R.id.title);
+        // if (tt != null) {
+        // tt.setText(o);
+        // }
+        // for(String value : targets) {
+        // if(value.equals("" + position)) {
+        // tt.setTextColor(Color.BLACK);
+        // }
+        // }
+        // }
+
+        String str = _items[position];
+        if (str != null) {
+            TextView title = (TextView) v.findViewById(R.id.title);
+            if (title != null) {
+                title.setText(str);
+            }
+        }
+        Integer image = _images[position];
+        ImageView imageRow = (ImageView) v.findViewById(R.id.icon);
+        imageRow.setImageResource(image);
+
+        return v;
+    }
+
+}
