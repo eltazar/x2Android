68cedd3b53ae99306bf2b19b094302e881a9b9c1
Mario Greco <mrgreco3@gmail.com>
2012-11-03 16:37:23 +0100

    [CategoryListAdapter]: pulizia

--- src/it/wm/android/adaptor/CategoryListAdapter.java
+++ src/it/wm/android/adaptor/CategoryListAdapter.java
@@ -12,12 +12,12 @@ import android.widget.TextView;
 import it.wm.perdue.R;
 
 public class CategoryListAdapter extends BaseAdapter {
-
+    
     private LayoutInflater _inflater;
     private String[]       _items;
     private Context        _context;
     private Integer[]      _images;
-
+    
     public CategoryListAdapter(Context context, String[] categories, Integer[] images) {
         _inflater = (LayoutInflater)
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -25,44 +25,31 @@ public class CategoryListAdapter extends BaseAdapter {
         _context = context;
         _images = images;
     }
-
+    
     @Override
     public int getCount() {
         return _items.length;
     }
-
+    
     @Override
     public Object getItem(int position) {
         return _items[position];
     }
-
+    
     @Override
     public long getItemId(int position) {
         return position + 1;
     }
-
+    
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
+        
         View v = convertView;
         if (v == null) {
             v = ((LayoutInflater) _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
                     .inflate(R.layout.category_row, null);
         }
-
-        // String o = (String)_items.getItem(position);
-        // if (o != null) {
-        // TextView tt = (TextView) v.findViewById(R.id.title);
-        // if (tt != null) {
-        // tt.setText(o);
-        // }
-        // for(String value : targets) {
-        // if(value.equals("" + position)) {
-        // tt.setTextColor(Color.BLACK);
-        // }
-        // }
-        // }
-
+        
         String str = _items[position];
         if (str != null) {
             TextView title = (TextView) v.findViewById(R.id.title);
@@ -73,8 +60,8 @@ public class CategoryListAdapter extends BaseAdapter {
         Integer image = _images[position];
         ImageView imageRow = (ImageView) v.findViewById(R.id.icon);
         imageRow.setImageResource(image);
-
+        
         return v;
     }
-
+    
 }
