83b99ad2644333234459fe0bbd703c62be7e2a42
Mario Greco <mrgreco3@gmail.com>
2012-11-03 17:18:53 +0100

    [ListAdapters]: creata classe CustomListAdapter astratta per la gestione dei vari adapters dell'app

--- src/it/wm/android/adaptor/NewsListAdapter.java
+++ src/it/wm/android/adaptor/NewsListAdapter.java
@@ -5,19 +5,14 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import it.wm.perdue.R;
 
-public class CategoryListAdapter extends CustomListAdapter {
+public class NewsListAdapter extends CustomListAdapter {
     
-    private Integer[] _images;
-    
-    public CategoryListAdapter(Context context, String[] categories, Integer[] images) {
-        
+    public NewsListAdapter(Context context, String[] categories) {
         super(context, categories);
-        _images = images;
     }
     
     @Override
@@ -26,19 +21,16 @@ public class CategoryListAdapter extends CustomListAdapter {
         View v = convertView;
         if (v == null) {
             v = ((LayoutInflater) _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-                    .inflate(R.layout.category_row, null);
+                    .inflate(R.layout.news_row, null);
         }
         
         String str = _items[position];
         if (str != null) {
-            TextView title = (TextView) v.findViewById(R.id.title);
+            TextView title = (TextView) v.findViewById(R.id.newsTitle);
             if (title != null) {
                 title.setText(str);
             }
         }
-        Integer image = _images[position];
-        ImageView imageRow = (ImageView) v.findViewById(R.id.icon);
-        imageRow.setImageResource(image);
         
         return v;
     }
