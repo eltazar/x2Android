e9ddff139f0523edeb8cdbeb9e0f09884fd841a0
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-11-04 19:44:58 +0100

    [NewsFragment] Usa JSONListAdaptor

--- src/it/wm/perdue/NewsFragment.java
+++ src/it/wm/perdue/NewsFragment.java
@@ -2,29 +2,37 @@
 package it.wm.perdue;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 
-import it.wm.android.adaptor.CustomListAdapter;
-import it.wm.android.adaptor.NewsListAdapter;
+import it.wm.HTTPAccess;
+import it.wm.android.adaptor.JSONListAdapter;
+import it.wm.perdue.businessLogic.Notizia;
 
-public class NewsFragment extends SherlockListFragment {
-    private static final String[] categories = {
-            "Ristorantiaaaaa aaaaaaa aaaaaaaaaaa aaaaa aaaaaa aaaaaa", "Pubs e Bar",
-            "Cinema", "Teatri",
-            "Musei", "Librerie",
-            "Benessere", "Parchi",
-            "Viaggi", "Altro"
-                                             };
+import java.util.HashMap;
+
+public class NewsFragment extends SherlockListFragment implements HTTPAccess.ResponseListener {
+    private static final String      DEBUG_TAG  = "NewsFragment";
+    private JSONListAdapter<Notizia> adapter    = null;
+    private HTTPAccess               httpAccess = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CustomListAdapter myListAdapter = new NewsListAdapter(getActivity(), categories);
-        setListAdapter(myListAdapter);
+        adapter = new JSONListAdapter<Notizia>(getActivity(), R.layout.news_row, R.id.newsTitle);
+        setListAdapter(adapter);
+        
+        httpAccess = new HTTPAccess();
+        String urlString = "http://www.cartaperdue.it/partner/v2.0/News.php";
+        HashMap<String, String> postMap = new HashMap<String, String>();
+        postMap.put("from", "0");
+        postMap.put("newJsonFormat", "1");
+        httpAccess.setResponseListener(this);
+        httpAccess.startHTTPConnection(urlString, HTTPAccess.Method.POST, postMap, null);
     }
     
     @Override
@@ -34,5 +42,15 @@ public class NewsFragment extends SherlockListFragment {
                 getActivity(),
                 getListView().getItemAtPosition(position).toString(),
                 Toast.LENGTH_SHORT).show();
+    }
+    
+    @Override
+    public void onHTTPResponseReceived(String tag, String response) {
+        adapter.addFromJSON(response);
+    }
+    
+    @Override
+    public void onHTTPerror(String tag) {
+        Log.d(DEBUG_TAG, "Errore nel download");
     }
 }
