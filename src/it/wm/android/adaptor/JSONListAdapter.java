2887b30bf0aecdbd47e012be33cbc70c0d3d2d9b
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-11-05 11:12:59 +0100

    [JSONListAdapter] No, forse il parsing del json non è poi così pesante. Il magic me lo fa senza colpo ferire, e stiamo parlando del *primo* dispositivo Android uscito [vabbè il secondo, ma l'hardware è praticamente identico]. Quindi niente thread apposito. Nice side effects: La cosa semplifica enormemente il ripristino dello stato della listView, dato che così nel momento del ripristino dello stato i dati sono già tutti in listview. Nel caso di cache hit i dati vengono ritornati immediatamente, startHttpConnection richiama direttamente il metodo del Listener quindi c'è garanzia di ordine delle chiamate.

--- src/it/wm/android/adaptor/JSONListAdapter.java
+++ src/it/wm/android/adaptor/JSONListAdapter.java
@@ -1,8 +1,8 @@
 
 package it.wm.android.adaptor;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.widget.ArrayAdapter;
 
 import com.google.gson.Gson;
@@ -30,24 +30,37 @@ public class JSONListAdapter<T> extends ArrayAdapter<T> {
             builder.replace(start, end, "]");
         }
         
-        new AsyncTask<String, Void, T[]>() {
-            
-            @Override
-            protected T[] doInBackground(String... params) {
-                GsonBuilder gsonBuilder = new GsonBuilder();
-                gsonBuilder.setDateFormat("yyyy-MM-dd kk:mm:ss");
-                onRegisterDeserializers(gsonBuilder);
-                Gson gson = gsonBuilder.create();
-                return gson.fromJson(params[0], JSONListAdapter.this.clazz);
-            }
-            
-            protected void onPostExecute(T[] result) {
-                JSONListAdapter.this.addAll(result);
-            }
-            
-        }.execute(builder.toString());
+        /*
+         * new AsyncTask<String, Void, T[]>() {
+         * @Override protected T[] doInBackground(String... params) {
+         * GsonBuilder gsonBuilder = new GsonBuilder();
+         * gsonBuilder.setDateFormat("yyyy-MM-dd kk:mm:ss");
+         * onRegisterDeserializers(gsonBuilder); Gson gson =
+         * gsonBuilder.create(); return gson.fromJson(params[0],
+         * JSONListAdapter.this.clazz); } protected void onPostExecute(T[]
+         * result) { JSONListAdapter.this.addAll(result); }
+         * }.execute(builder.toString());
+         */
+        
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("yyyy-MM-dd kk:mm:ss");
+        onRegisterDeserializers(gsonBuilder);
+        Gson gson = gsonBuilder.create();
+        T[] objects = gson.fromJson(builder.toString(), JSONListAdapter.this.clazz);
+        this.supportAddAll(objects);
     }
     
     protected void onRegisterDeserializers(GsonBuilder gsonBuilder) {
+    }
+    
+    @SuppressLint("NewApi")
+    public void supportAddAll(T... items) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+            super.addAll(items);
+        } else {
+            for (T item : items) {
+                super.add(item);
+            }
+        }
     }
 }
