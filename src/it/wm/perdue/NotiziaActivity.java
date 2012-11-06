3b98838685eee155750bfbf21fef5c268d1921cf
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-11-06 03:53:39 +0100

    [NotiziaActivity] Utilizza Gson e salva lo stato della webview

--- src/it/wm/perdue/NotiziaActivity.java
+++ src/it/wm/perdue/NotiziaActivity.java
@@ -10,62 +10,53 @@ import android.widget.TextView;
 import it.wm.HTTPAccess;
 import it.wm.perdue.businessLogic.Notizia;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class NotiziaActivity extends Activity implements HTTPAccess.ResponseListener {
     
     private static final String DEBUG_TAG  = "NotiziaActivity";
     private HTTPAccess          httpAccess = null;
-    private String              urlString;
+    private String              urlString  = null;
+    private Notizia             notizia    = null;
+    private WebView             webView    = null;
     
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        notizia = (Notizia) getIntent().getSerializableExtra("notizia");
+        
         setContentView(R.layout.notizia);
-        
-        httpAccess = new HTTPAccess();
-        httpAccess.setResponseListener(this);
-        
-        Notizia notizia = (Notizia) getIntent().getSerializableExtra("notizia");
         TextView title = (TextView) findViewById(R.id.title);
         title.setText(notizia.getTitolo());
+        webView = (WebView) findViewById(R.id.newsWebView);
         
-        urlString = "http://www.cartaperdue.it/partner/Notizia.php?id=" + notizia.getId();
-        
-        httpAccess.startHTTPConnection(urlString, HTTPAccess.Method.GET, null, null);
-        
-        Log.d(DEBUG_TAG, "id " + notizia.getId());
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState);
+        } else {
+            httpAccess = new HTTPAccess();
+            httpAccess.setResponseListener(this);
+            urlString = "http://www.cartaperdue.it/partner/Notizia.php?id=" + notizia.getId();
+            httpAccess.startHTTPConnection(urlString, HTTPAccess.Method.GET, null, null);
+            Log.d(DEBUG_TAG, "id " + notizia.getId());
+        }
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
     }
     
     @Override
     public void onHTTPResponseReceived(String tag, String response) {
-        // TODO Auto-generated method stub
-        Log.d(DEBUG_TAG, "RICEVUTA RISPOSTA");
-        JSONObject mainObject = null;
-        try {
-            
-            mainObject = new JSONObject(response);
-            
-            Log.d(DEBUG_TAG,
-                    "***** object:"
-                            + mainObject.getJSONArray("Esercente").getJSONObject(0)
-                                    .getString("post_content"));
-            WebView webView = (WebView) findViewById(R.id.newsWebView);
-            webView.loadData(mainObject.getJSONArray("Esercente").getJSONObject(0)
-                    .getString("post_content"), "text/html; charset=UTF-8", null);
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.getLocalizedMessage();
-        }
-        // JSONObject esercente = mainObject.getJSONObject("Esercente");
-        // String post_content = esercente.getString("post_content");
+        Log.d(DEBUG_TAG, "RICEVUTA RISPOSTA: " + response);
+        response = Utils.stripEsercente(response);
+        notizia = Utils.getGson().fromJson(response, Notizia[].class)[0];
+        
+        Log.d(DEBUG_TAG, "***** object: " + notizia.getTesto());
+        webView.loadData(notizia.getTesto(), "text/html; charset=UTF-8", null);
         
     }
     
     @Override
     public void onHTTPerror(String tag) {
-        // TODO Auto-generated method stub
-        
     }
     
 }
