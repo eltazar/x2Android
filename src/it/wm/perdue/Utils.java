ecef19d3a4c1bd575dec8ff2f291769eaf504084
Mario Greco <mrgreco3@gmail.com>
2012-11-10 23:43:06 +0100

    [Utils]: ora stripEsercente riconosce quale tipo di stringa "esercente…" è presente nel json

--- src/it/wm/perdue/Utils.java
+++ src/it/wm/perdue/Utils.java
@@ -4,6 +4,8 @@
 
 package it.wm.perdue;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -11,12 +13,31 @@ import com.google.gson.GsonBuilder;
  * @author Gabriele "Whisky" Visconti
  */
 public final class Utils {
+    
     public static String stripEsercente(CharSequence c) {
         StringBuilder builder = new StringBuilder(c);
-        if (builder.substring(0, 13).equals("{\"Esercente\":")) {
+        
+        if (builder.length() >= 13 && builder.substring(0, 13).equals("{\"Esercente\":")) {
             builder.delete(0, 13);
             builder.deleteCharAt(builder.length() - 1);
         }
+        
+        if (builder.length() >= 23 && builder.substring(0, 23).equals("{\"Esercente:FirstRows\":")) {
+            Log.d("UTILS", "SUBSTRING firstRows = " + builder.substring(0, 23));
+            builder.delete(0, 23);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        if (builder.length() >= 22 && builder.substring(0, 22).equals("{\"Esercente:MoreRows\":")) {
+            Log.d("UTILS", "SUBSTRING moreRows = " + builder.substring(0, 22));
+            builder.delete(0, 22);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        if (builder.length() >= 20 && builder.substring(0, 20).equals("{\"Esercente:Search\":")) {
+            Log.d("UTILS", "SUBSTRING search = " + builder.substring(0, 20));
+            builder.delete(0, 20);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        
         return builder.toString();
     }
     
