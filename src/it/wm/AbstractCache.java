9402e1bfaee48a3871f9aa7aa715717ac1b9adab
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2012-11-11 22:54:24 +0100

    [AbstractCache] NonBugFix: ogni tanto sbuca una cache line con data a null, e listeners a null, cosa che in teoria non dovrebbe succedere. Così facendo non risolviamo il problema, ma almeno facciamo in modo che l'app non crashi ogni tre per due, e possiamo continuare lo sviluppo. Aggiornamenti nelle prossime puntate.

--- src/it/wm/AbstractCache.java
+++ src/it/wm/AbstractCache.java
@@ -19,13 +19,13 @@ abstract class AbstractCache<T> implements DownloaderTask.DownloadListener {
     static final int                               CACHE_PENDING = 2;
     static final int                               CACHE_MISS    = 3;
     private HashMap<DownloadRequest, CacheLine<T>> cache         = null;
-
+    
     protected AbstractCache() {
         if (cache == null) {
             cache = new HashMap<DownloadRequest, CacheLine<T>>();
         }
     }
-
+    
     protected int getCacheLineStatus(DownloadRequest key) {
         Log.d(DEBUG_TAG, "**getCacheLineStatus");
         CacheLine<T> line = cache.get(key);
@@ -33,22 +33,24 @@ abstract class AbstractCache<T> implements DownloaderTask.DownloadListener {
             Log.d(DEBUG_TAG, "**getCacheLineStatus: MISS");
             return CACHE_MISS;
         } // line != null
-        if (line.data == null) {
+          // TODO: IMPORTANTE - se data non è null listeners dovrebbe esserlo
+          // SEMPRE, invece evidentemente non succede... perché???
+        if (line.data == null && line.listeners != null) {
             Log.d(DEBUG_TAG, "**getCacheLineStatus: PENDING");
             return CACHE_PENDING;
         } else {
             Log.d(DEBUG_TAG, "**getCacheLineStatus: HIT");
             return CACHE_HIT;
         }
-
+        
     }
-
+    
     public T getCacheLine(DownloadRequest params, CacheListener<T> l) {
         Log.d(DEBUG_TAG, "**getCacheLine");
         switch (getCacheLineStatus(params)) {
             case CACHE_HIT:
                 return cache.get(params).data;
-
+                
             case CACHE_PENDING:
                 CacheLine<T> line = cache.get(params);
                 if (!line.listeners.contains(l)) {
@@ -56,7 +58,7 @@ abstract class AbstractCache<T> implements DownloaderTask.DownloadListener {
                     line.listeners.add(l);
                 }
                 return null;
-
+                
             case CACHE_MISS:
                 CacheLine<T> newLine = new CacheLine<T>();
                 if (!newLine.listeners.contains(l)) {
@@ -68,21 +70,21 @@ abstract class AbstractCache<T> implements DownloaderTask.DownloadListener {
                 task.setListener(this);
                 task.execute(params);
                 return null;
-
+                
             default:
                 return null;
         }
     }
-
+    
     protected abstract T convertData(byte[] data);
-
+    
     public void removeListener(DownloadRequest request, CacheListener<T> l) {
         CacheLine<T> line = cache.get(request);
         if (line != null && line.listeners != null) {
             line.listeners.remove(l);
         }
     }
-
+    
     /* *** BEGIN: DownloaderTask.ResponseListener **************** */
     @Override
     public void onDownloadCompleted(DownloadRequest request, byte[] responseBody) {
@@ -95,7 +97,7 @@ abstract class AbstractCache<T> implements DownloaderTask.DownloadListener {
         line.listeners.clear();
         line.listeners = null;
     }
-
+    
     @Override
     public void onDownloadError(DownloadRequest request) {
         Log.d(DEBUG_TAG, "**onHTTPerror");
@@ -107,22 +109,22 @@ abstract class AbstractCache<T> implements DownloaderTask.DownloadListener {
         line.listeners = null;
         cache.remove(request);
     }
-
+    
     /* *** END: DownloaderTask.ResponseListener **************** */
-
+    
     private class CacheLine<K> {
         public T                      data = null;
         public List<CacheListener<K>> listeners;
-
+        
         public CacheLine() {
             listeners = new ArrayList<CacheListener<K>>();
         }
-
+        
     }
-
+    
     public interface CacheListener<J> {
         public void onCacheLineLoaded(DownloadRequest request, J data);
-
+        
         public void onCacheLineError(DownloadRequest request);
     }
 }
