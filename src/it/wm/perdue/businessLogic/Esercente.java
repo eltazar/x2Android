81ccfb13038ecbdebaa4555fa5c5cccfe1b0bec3
Mario Greco <mrgreco3@gmail.com>
2012-11-28 12:39:10 +0100

    [Esercente]: aggiunto array giorni di validità

--- src/it/wm/perdue/businessLogic/Esercente.java
+++ src/it/wm/perdue/businessLogic/Esercente.java
@@ -8,38 +8,47 @@ import com.google.gson.annotations.SerializedName;
  */
 public class Esercente implements HasID {
     @SerializedName("IDesercente")
-    private int     id             = -1;
+    private int      id             = -1;
     @SerializedName("Insegna_Esercente")
-    private String  insegna        = null;
+    private String   insegna        = null;
     @SerializedName("Indirizzo_Esercente")
-    private String  indirizzo      = null;
+    private String   indirizzo      = null;
     @SerializedName("Citta_Esercente")
-    private String  citta          = null;
+    private String   citta          = null;
     @SerializedName("Zona_Esercente")
-    private String  zona           = null;
+    private String   zona           = null;
     @SerializedName("Telefono_Esercente")
-    private String  telefono       = null;
+    private String   telefono       = null;
     @SerializedName("Email_Esercente")
-    private String  email          = null;
+    private String   email          = null;
     @SerializedName("Url_Esercente")
-    private String  url            = null; // TODO: cambiare classe in URL
+    private String   url            = null; // TODO: cambiare classe in URL
     @SerializedName("Giorno_chiusura_Esercente")
-    private String  giornoChiusura = null;
+    private String   giornoChiusura = null;
+    @SerializedName("Giorni")
+    private String[] giorni         = null;
     @SerializedName("Tipo_Teser")
-    private String  tipologia      = null;
+    private String   tipologia      = null;
     @SerializedName("Note_Varie_CE")
-    private String  noteVarie      = null; // TODO: riguardarsi cosa contiene e
-                                            // cambiare nome.
+    private String   noteVarie      = null; // TODO: riguardarsi cosa contiene
+                                             // e
+                                             // cambiare nome. --> dovrebbe
+                                             // contenere la condizione dello
+                                             // sconto
     @SerializedName("Ulteriori_Informazioni")
-    private boolean ulterioriInfo  = false;
+    private boolean  ulterioriInfo  = false;
     @SerializedName("Esercente_Virtuale")
-    private boolean virtuale       = false;
+    private boolean  virtuale       = false;
     @SerializedName("Latitudine")
-    private double  latitude       = 0.0;
+    private double   latitude       = 0.0;
     @SerializedName("Longitudine")
-    private double  longitude      = 0.0;
+    private double   longitude      = 0.0;
     @SerializedName("Distanza")
-    private double  distanza       = 0.0;
+    private double   distanza       = 0.0;
+    
+    public String[] getGiorni() {
+        return giorni;
+    }
     
     public int getID() {
         return id;
