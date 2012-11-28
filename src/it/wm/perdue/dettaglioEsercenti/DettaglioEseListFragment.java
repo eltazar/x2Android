f512d3335015d7eab7e63c283dc6f7ff58e61268
Mario Greco <mrgreco3@gmail.com>
2012-11-28 21:38:12 +0100

    [DettaglioEseListFragment]: aggiunto adapter per mostrare dati esercente

--- src/it/wm/perdue/dettaglioEsercenti/DettaglioEseListFragment.java
+++ src/it/wm/perdue/dettaglioEsercenti/DettaglioEseListFragment.java
@@ -9,34 +9,37 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 
 import it.wm.HTTPAccess;
-import it.wm.perdue.JSONListAdapter;
 import it.wm.perdue.R;
+import it.wm.perdue.Utils;
 import it.wm.perdue.businessLogic.Esercente;
+import it.wm.perdue.businessLogic.HasID;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DettaglioEseListFragment extends SherlockListFragment implements
         HTTPAccess.ResponseListener {
-    private static final String                    DEBUG_TAG  = "DettaglioEseListFragment";
-    protected static final String                  TAG_NORMAL = "normal";
-    protected static final String                  ESE_ID     = "eseId";
+    private static final String       DEBUG_TAG  = "DettaglioEseListFragment";
+    protected static final String     TAG_NORMAL = "normal";
+    protected static final String     ESE_ID     = "eseId";
     
     // Gestione dei download:
-    protected HTTPAccess                           httpAccess = null;
-    protected String                               urlString  = null;
-    protected HashMap<String, String>              postMap    = null;
+    protected HTTPAccess              httpAccess = null;
+    protected String                  urlString  = null;
+    protected HashMap<String, String> postMap    = null;
     
     // Gestione dello stato della lista:
-    protected JSONListAdapter<? extends Esercente> adapter    = null;
-    private Parcelable                             listState  = null;
+    protected ArrayAdapter<Esercente> adapter    = null;
+    private Parcelable                listState  = null;
     
-    private String                                 eseId      = null;
+    private String                    eseId      = null;
     
     public static DettaglioEseListFragment newInstance(String eseId) {
         DettaglioEseListFragment fragment = new DettaglioEseListFragment();
@@ -70,17 +73,14 @@ public class DettaglioEseListFragment extends SherlockListFragment implements
         LayoutInflater inflater = (LayoutInflater) getActivity()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         
-        setListAdapter(adapter);
-        
         httpAccess.startHTTPConnection(urlString, HTTPAccess.Method.GET,
                 null, TAG_NORMAL);
     }
     
     protected void onCreateAdapters() {
-        adapter = new DettaglioEsercenteJSONListAdapter(
-                getActivity(),
-                R.layout.esercente_row,
-                Esercente[].class);
+        // adapter = new DettaglioEsercenteJSONListAdapter(
+        // getActivity(),
+        // R.layout.esercente_row);
     }
     
     @Override
@@ -103,14 +103,18 @@ public class DettaglioEseListFragment extends SherlockListFragment implements
         // downloading--;
         
         Log.d("XXX", "RISPOSTA = " + response);
+        Esercente esercente = null;
         
-        int n;
         if (tag.equals(TAG_NORMAL)) {
             // Se riceviamo un risultato non di ricerca, lo aggiungiamo sempre e
             // comunque:
-            n = adapter.addFromJSON(response);
-            
+            esercente = Utils.getEsercenteFromJSON(response);
+            adapter = new DettaglioEsercenteAdapter<Esercente>(
+                    getActivity(),
+                    R.layout.esercente_row, esercente);
+            setListAdapter(adapter);
         }
+        
     }
     
     @Override
@@ -126,33 +130,135 @@ public class DettaglioEseListFragment extends SherlockListFragment implements
         
     }
     
-    private static class DettaglioEsercenteJSONListAdapter extends JSONListAdapter<Esercente> {
+    private static class DettaglioEsercenteAdapter<T extends HasID> extends ArrayAdapter<Esercente> {
         
-        public DettaglioEsercenteJSONListAdapter(Context context, int resource,
-                Class<Esercente[]> clazz) {
-            super(context, resource, clazz);
+        private Esercente         esercente = null;
+        private ArrayList<String> sections  = null;
+        
+        public DettaglioEsercenteAdapter(Context context, int resource, Esercente esercente) {
+            super(context, resource);
+            // TODO: creare datamodel da esercente
+            this.esercente = esercente;
+            Log.d("XXX", "DETTAGLIO ESERCENTE ADAPTER --> " + this.esercente.getID());
+            sections = new ArrayList<String>();
+            checkFields();
         }
         
         public View getView(int position, View convertView, ViewGroup parent) {
             
             View v = convertView;
+            int resource = 0;
+            TextView textView = null;
+            TextView contactTextView = null;
+            TextView kindContactTextView = null;
+            
             if (v == null) {
+                
+                if (sections.get(position).equals("info")) {
+                    resource = R.layout.dettaglio_info_row;
+                }
+                else if (sections.get(position).equals("map")) {
+                    resource = R.layout.map_row;
+                }
+                else if (sections.get(position).equals("tel")
+                        || sections.get(position).equals("mail") ||
+                        sections.get(position).equals("url")) {
+                    resource = R.layout.contact_row;
+                }
+                else if (sections.get(position).equals("altre")) {
+                    resource = R.layout.contact_row;
+                }
+                
                 v = ((LayoutInflater) super.getContext().getSystemService(
                         Context.LAYOUT_INFLATER_SERVICE))
-                        .inflate(R.layout.esercente_row, null);
+                        .inflate(resource, null);
             }
             
-            Esercente esercente = getItem(position);
-            
-            // prova html tag
             if (esercente != null) {
-                TextView title = (TextView) v.findViewById(R.id.eseTitle);
                 
-                title.setText(Html.fromHtml("<b>" + esercente.getCitta() + "</b>" + "<br />" +
-                        "<small>" + esercente.getIndirizzo() + "</small>" + "<br />" +
-                        "<small>" + esercente.getInsegna() + "</small>"));
+                // title.setText(Html.fromHtml("<b>" + esercente.getCitta() +
+                // "</b>" + "<br />" +
+                // "<small>" + esercente.getIndirizzo() + "</small>" + "<br />"
+                // +
+                // "<small>" + esercente.getInsegna() + "</small>"
+                // + "<br />" +
+                // "<small>" + esercente.getNoteVarie() + "</small>"));
+                
+                if (sections.get(position).equals("info")) {
+                    textView = (TextView) v.findViewById(R.id.infoRow);
+                    textView.setText(Html.fromHtml("<b> Giorno di chiusura</b>" + "<br />" +
+                            esercente.getGiornoChiusura() + "<br />" +
+                            "<b> Giorno di validitˆ </b>" + "<br />" + esercente.getGiorniString()
+                            + "<br />" + "<b> Condizioni</b>" + "<br />" + esercente.getNoteVarie()));
+                    
+                }
+                else if (sections.get(position).equals("map")) {
+                    textView = (TextView) v.findViewById(R.id.mapInfo);
+                    textView.setText(Html.fromHtml("<b>Cittˆ</b>" + "<br />" +
+                            esercente.getCitta() + "<br />" +
+                            "<b> Zona </b>" + "<br />" + esercente.getZona()
+                            + "<br />" + "<b> Indirizzo</b>" + "<br />" + esercente.getIndirizzo()));
+                }
+                else if (sections.get(position).equals("tel")) {
+                    contactTextView = (TextView) v.findViewById(R.id.contactResource);
+                    kindContactTextView = (TextView) v.findViewById(R.id.contactKind);
+                    contactTextView.setText(esercente.getTelefono());
+                    kindContactTextView.setText("Telefono");
+                }
+                else if (sections.get(position).equals("mail")) {
+                    contactTextView = (TextView) v.findViewById(R.id.contactResource);
+                    kindContactTextView = (TextView) v.findViewById(R.id.contactKind);
+                    contactTextView.setText(esercente.getEmail());
+                    kindContactTextView.setText("E-mail");
+                }
+                else if (sections.get(position).equals("url")) {
+                    contactTextView = (TextView) v.findViewById(R.id.contactResource);
+                    kindContactTextView = (TextView) v.findViewById(R.id.contactKind);
+                    contactTextView.setText(esercente.getUrl());
+                    kindContactTextView.setText("Sito web");
+                }
+                else if (sections.get(position).equals("altre")) {
+                    contactTextView = (TextView) v.findViewById(R.id.contactResource);
+                    kindContactTextView = (TextView) v.findViewById(R.id.contactKind);
+                    contactTextView.setText(esercente.getTelefono());
+                    kindContactTextView.setText("PROVA ALTRO");
+                }
             }
             return v;
+        }
+        
+        @Override
+        public int getCount() {
+            Log.d("XXX", "COUNT = " + sections.size());
+            // return hashMap.get("count");
+            return sections.size();
+        }
+        
+        private void checkFields() {
+            
+            if (esercente.getGiorni() != null || esercente.getGiornoChiusura() != null ||
+                    esercente.getNoteVarie() != null) {
+                sections.add("info");
+            }
+            if (esercente.getCitta() != null || esercente.getZona() != null
+                    || esercente.getIndirizzo() != null) {
+                sections.add("map");
+            }
+            if (esercente.isUlterioriInfo()) {
+                sections.add("altre");
+            }
+            
+            if (esercente.getUrl() != null) {
+                sections.add("url");
+            }
+            if (esercente.getEmail() != null) {
+                sections.add("mail");
+            }
+            
+            if (esercente.getTelefono() != null) {
+                sections.add("tel");
+            }
+            
         }
     }
 }
