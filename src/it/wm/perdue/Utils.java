0f9d53c98074a07284224fd08d6e7155df289d47
Mario Greco <mrgreco3@gmail.com>
2012-11-29 01:10:39 +0100

    [Utils]: aggiunto metodo per ombreggiatura immagini

--- src/it/wm/perdue/Utils.java
+++ src/it/wm/perdue/Utils.java
@@ -6,6 +6,12 @@ package it.wm.perdue;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.util.Log;
 
 import com.google.android.maps.GeoPoint;
@@ -177,4 +183,46 @@ public final class Utils {
         
         return esercente;
     }
+    
+    public static Bitmap getDropShadow3(Bitmap bitmap) {
+        
+        if (bitmap == null)
+            return null;
+        int think = 6;
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        
+        int newW = w - (think);
+        int newH = h - (think);
+        
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+        Bitmap bmp = Bitmap.createBitmap(w, h, conf);
+        Bitmap sbmp = Bitmap.createScaledBitmap(bitmap, newW, newH, false);
+        
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        Canvas c = new Canvas(bmp);
+        
+        // Right
+        Shader rshader = new LinearGradient(newW, 0, w, 0, Color.GRAY, Color.LTGRAY,
+                Shader.TileMode.CLAMP);
+        paint.setShader(rshader);
+        c.drawRect(newW, think, w, newH, paint);
+        
+        // Bottom
+        Shader bshader = new LinearGradient(0, newH, 0, h, Color.GRAY, Color.LTGRAY,
+                Shader.TileMode.CLAMP);
+        paint.setShader(bshader);
+        c.drawRect(think, newH, newW, h, paint);
+        
+        // Corner
+        Shader cchader = new LinearGradient(0, newH, 0, h, Color.LTGRAY, Color.LTGRAY,
+                Shader.TileMode.CLAMP);
+        paint.setShader(cchader);
+        c.drawRect(newW, newH, w, h, paint);
+        
+        c.drawBitmap(sbmp, 0, 0, null);
+        
+        return bmp;
+    }
+    
 }
