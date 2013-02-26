f990e89a9b488b1d5d9e393e55a6fb7590563d4a
eltazar <eltazar@eltazar.(none)>
2013-02-26 18:22:14 +0100

    [Coupon]: sistemata la classe di buisness logic

--- src/it/wm/perdue/businessLogic/Coupon.java
+++ src/it/wm/perdue/businessLogic/Coupon.java
@@ -6,7 +6,6 @@ package it.wm.perdue.businessLogic;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.net.URL;
 import java.util.Date;
 
 /**
@@ -16,39 +15,43 @@ public class Coupon implements HasID {
     @SerializedName("idofferta")
     private int     id                 = -1;
     @SerializedName("offerta_title")
-    private String  titolo             = null;
+    private String  titolo             = null; //questo è un po più lungo e va dove ci pare
     @SerializedName("offerta_titolo_breve")
-    private String  titoloBreve        = null;
+    private String  titoloBreve        = null; //questo va nell'header della view
     @SerializedName("offerta_foto_big")
-    private URL     urlImmagine        = null;
+    private String     urlImmagine        = null;
     @SerializedName("coupon_valore_acquisto")
-    private float   valoreAcquisto     = 0;
+    private float   valoreAcquisto     = 0; //prezzo di acquisto del coupon
     @SerializedName("coupon_valore_facciale")
-    private float   valoreFacciale     = 0;
+    private float   valoreFacciale     = 0; //prezzo non scontato, pieno
     @SerializedName("offerta_sconto_va")
-    private float   sconto             = 0;
+    private float   sconto             = 0; //questoi è quanti euro si risparmia
     @SerializedName("offerta_sconto_per")
-    private String  scontoPer          = null; // ????????
+    private String  scontoPer          = null; // questa è la percentuale di risparmio
     @SerializedName("offerta_descrizione_estesa")
-    private String  descrizione        = null;
-    @SerializedName("offerta_descrizione_breve")
-    private String  descrizioneBreve   = null;
-    @SerializedName("offerta_condizioni_sintetiche")
+    private String  descrizioneEstesa        = null; // questo va in "per saperne di più", e praticamente e è null non va mostrata la cella
+    @SerializedName("offerta_descrizione_breve") 
+    private String  descrizioneBreve   = null; //questo in dettaglio offerta
+    @SerializedName("offerta_condizioni_sintetiche") // questo in termini e condizioni
     private String  condizioni         = null;
     @SerializedName("coupon_periodo_dal")
-    private Date    couponPeriodoDal   = null; // ?????????
+    private Date    couponPeriodoDal   = null; // bho, forse inizio dell'offerta
     @SerializedName("offerta_pediodo_dal")
     private Date    inizioValidita     = null;
     @SerializedName("offerta_periodo_al")
-    private Date    fineValidita       = null;
+    private Date    fineValidita       = null; //quando scade l'offerta
     @SerializedName("idesercente")
     private int     idEsercente        = -1;
     @SerializedName("esercente_nome")
     private String  nomeEsercente      = null;
     @SerializedName("esercente_indirizzo")
     private String  indirizzoEsercente = null;
+    @SerializedName("IdTipologia_Esercente")
+    private int  idTipoEsercente = -1;
+    //@SerializedName("IDcontratto_Contresercente")
+    //private int  idContrattoControes = -1;
     @SerializedName("esercente_comune")
-    private boolean esercenteComune    = false;
+    private String esercenteComune    = null;
     
     public int getID() {
         return id;
@@ -57,12 +60,20 @@ public class Coupon implements HasID {
     public String getTitolo() {
         return titolo;
     }
+    /*
+    public int getIdContrattoControes(){
+        return idContrattoControes;
+    }*/
+    
+    public int idTipoEsercente(){
+        return idTipoEsercente;
+    }
     
     public String getTitoloBreve() {
         return titoloBreve;
     }
     
-    public URL getUrlImmagine() {
+    public String getUrlImmagine() {
         return urlImmagine;
     }
     
@@ -83,7 +94,7 @@ public class Coupon implements HasID {
     }
     
     public String getDescrizione() {
-        return descrizione;
+        return descrizioneEstesa;
     }
     
     public String getDescrizioneBreve() {
@@ -118,8 +129,7 @@ public class Coupon implements HasID {
         return indirizzoEsercente;
     }
     
-    public boolean isEsercenteComune() {
+    public String getEsercenteComune() {
         return esercenteComune;
-    }
-    
+    }    
 }
