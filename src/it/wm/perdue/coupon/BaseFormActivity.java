bc18c148de00b406543286c8d5e8dae2863fb4d3
mario <eltazar@eltazar-kubuntu.(none)>
2013-03-02 00:58:08 +0100

    [BaseFormatActivity]: ora implementa il listener del dialog di recupero psw

--- src/it/wm/perdue/coupon/BaseFormActivity.java
+++ src/it/wm/perdue/coupon/BaseFormActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,23 +14,22 @@ import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.actionbarsherlock.view.Menu;
 
 import it.wm.HTTPAccess;
-import it.wm.perdue.R;
+import it.wm.perdue.coupon.RetrievePswDialog.RetrievePswListener;
 
 public class BaseFormActivity extends SherlockFragmentActivity implements
-        HTTPAccess.ResponseListener, OnClickListener, OnEditorActionListener, OnFocusChangeListener {
+        HTTPAccess.ResponseListener, OnClickListener, OnEditorActionListener, OnFocusChangeListener, RetrievePswListener {
     
     private static final String DEBUG_TAG  = "BaseFormActivity";
+    private static final String LOGIN_FRAG_TAG = "loginFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // setContentView(R.layout.dettaglio_ese_base_activity);
-        
+                
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
                       
@@ -48,61 +48,11 @@ public class BaseFormActivity extends SherlockFragmentActivity implements
             return;
         
         f = RegistrazioneFormFragment.newInstance();
-        
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction =
                 fragmentManager.beginTransaction();
-        fragmentTransaction.add(android.R.id.content, f);
+        fragmentTransaction.add(android.R.id.content,f, LOGIN_FRAG_TAG);//.add(android.R.id.content, f);
         fragmentTransaction.commit();
-        
-    }
-    
-    /* *** BEGIN: OptionsMenu Methods **************** */
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // mSearchView.setOnCloseListener(this);
-        
-//        if (isRisto) {
-//            getSupportMenuInflater().inflate(R.menu.dettaglio_ese_risto_menu, menu);
-//        }
-//        else {
-//            getSupportMenuInflater().inflate(R.menu.dettaglio_ese_menu, menu);
-//        }
-        
-        getSupportMenuInflater().inflate(R.menu.main_activity_menu, menu);
-        return true;
-    }
-    /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent = null;
-        
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                intent = NavUtils.getParentActivityIntent(this);
-                intent.putExtra(Intent.EXTRA_TEXT, MainActivity.DOVE_USARLA_TAB_TAG);
-                NavUtils.navigateUpTo(this, intent);
-                return true;
-            case R.id.commenti:
-                Log.d("BBB", "cliccato pulsante commenti, eseID = " + id);
-                Bundle extras = new Bundle();
-                // extras.putSerializable("notizia", (Serializable)
-                // l.getItemAtPosition(position));
-                intent = new Intent(this, CommentiBaseActivity.class);
-                extras.putString(CommentiBaseActivity.Tags.ID, id);
-                intent.putExtras(extras);
-                startActivity(intent);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-    
-    public static class Tags {
-        public static final String ID       = "id";
-        public static final String TITLE    = "title";
-        public static final String IS_RISTO = "isRisto";
     }
 
     @Override
@@ -134,4 +84,25 @@ public class BaseFormActivity extends SherlockFragmentActivity implements
         // TODO Auto-generated method stub
         
     }
+
+    /*RetrievePswListener
+     * */
+    @Override
+    public void onDialogPositiveClick(SherlockDialogFragment dialog, String retrieveString) {
+        Log.d("retrieve","cliccato avanti su dialog");    
+        RegistrazioneFormFragment loginFragment = (RegistrazioneFormFragment) getSupportFragmentManager().findFragmentByTag(LOGIN_FRAG_TAG);
+        try{
+            loginFragment.sendRetrieveRequest(retrieveString);
+        }
+        catch(NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDialogNegativeClick(SherlockDialogFragment dialog) {
+        Log.d("retrieve","cliccato annulla su dialog");               
+    }
+    /*RetrievePswListener END
+     * */
 }
