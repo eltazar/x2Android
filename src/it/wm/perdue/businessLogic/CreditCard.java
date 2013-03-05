ff9048fb8a5664f0ce98dbc83abdb223dd70d3e2
mario <eltazar@eltazar-kubuntu.(none)>
2013-03-05 13:52:57 +0100

    [CreditCard]: resa parcelable

--- src/it/wm/perdue/businessLogic/CreditCard.java
+++ src/it/wm/perdue/businessLogic/CreditCard.java
@@ -1,12 +1,20 @@
 package it.wm.perdue.businessLogic;
 
-public class CreditCard {
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+public class CreditCard implements Parcelable{
     private String owner;
     private String number;
     private String cvv;
     private int month;
     private int year;
+    private int institute;
     
+    public int getInstitute(){
+        return institute;
+    }
     public String getOwner(){
         return owner;
     }
@@ -23,6 +31,9 @@ public class CreditCard {
         return year;
     }
     
+    public void setInstitute(int i){
+        institute = i;
+    }
     public void setOwner(String o){
         owner = o;
     }
@@ -38,4 +49,58 @@ public class CreditCard {
     public void setYear(int y){
         year = y;
     }
+    
+    public String toString(){
+        return "CARTA DI CREDITO: n = "+number+" cvv = "+cvv+" owner = "+owner+" month"+month
+                +" year = "+year+" institute = "+institute;
+    }
+    
+    public CreditCard(){
+    }
+    
+    public CreditCard(Parcel in) {  
+        Log.d("check","creo carta di credito parcelable");
+        readFromParcel(in);  
+    }  
+    
+    @Override  
+    public void writeToParcel(Parcel out, int flags) {  
+        Log.d("check","write to parcel");
+        out.writeString(owner);  
+        out.writeString(number);  
+        out.writeString(cvv);
+        out.writeInt(month);
+        out.writeInt(year);
+        out.writeInt(institute); 
+    }  
+    
+    private void readFromParcel(Parcel in) {    
+        Log.d("check","read from to parcel");
+
+        owner = in.readString();  
+        number = in.readString();  
+        cvv = in.readString();  
+        month = in.readInt();
+        year = in.readInt();  
+        institute = in.readInt();
+    }  
+    
+    public static final Parcelable.Creator<CreditCard> CREATOR = new Parcelable.Creator<CreditCard>() {  
+        
+        public CreditCard createFromParcel(Parcel in) {  
+            return new CreditCard(in);  
+        }  
+        
+        public CreditCard[] newArray(int size) {  
+            return new CreditCard[size];  
+        }  
+        
+    };  
+    
+    @Override  
+    public int describeContents() {  
+        return 0;  
+    } 
+    
+    
 }
