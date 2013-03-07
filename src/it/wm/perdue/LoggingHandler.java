3535a8866c121777dab96254f0936fb64605a7b1
mario <eltazar@eltazar-kubuntu.(none)>
2013-03-08 00:20:16 +0100

    [LoggingHandler]: inserita interfaccia listener

--- src/it/wm/perdue/LoggingHandler.java
+++ src/it/wm/perdue/LoggingHandler.java
@@ -1,15 +1,29 @@
 package it.wm.perdue;
 
+import android.app.Activity;
+
 import it.wm.perdue.businessLogic.LoginData;
 
 public class LoggingHandler {
     
+    private static OnLoggingHandlerListener listener = null;
+    
+    public interface OnLoggingHandlerListener{
+        public void onDidLogin();
+        public void onDidLogout();
+    }
+    
+    public static void setListener(Activity l){
+        listener = (OnLoggingHandlerListener) l;
+    }
+    
     //salva i dati di login
-    public static void onDidLogin(LoginData l){
+    public static void doLogin(LoginData l){
         Utils.setPreferenceString("userName",l.getNomeContatto());
         Utils.setPreferenceString("userSurname",l.getCognomeContatto());
         Utils.setPreferenceString("userMail",l.getEmail());
         Utils.setPreferenceString("userId",l.getIdCustomer()+"");
+        listener.onDidLogin();
     }
     
     //recupera i dati di login
@@ -19,11 +33,12 @@ public class LoggingHandler {
                 Integer.parseInt(Utils.getPreferenceString("userId","-1")));
     }
     
-    public static void onDidLogout(){
+    public static void doLogout(){
         Utils.setPreferenceString("userName",null);
         Utils.setPreferenceString("userSurname",null);
         Utils.setPreferenceString("userMail",null);
         Utils.setPreferenceString("userId",-1+"");
+        listener.onDidLogout();
     }
     
     public static boolean isLogged(){
