63cd3ad62b3d4743fbd57fa3508a33b0078f7b85
mario <eltazar@eltazar-kubuntu.(none)>
2013-03-09 01:40:23 +0100

    [DetailCouponListFragment]: inziato a disegnare interfaccia coupon e popolarla coi dati

--- src/it/wm/perdue/coupon/DetailCouponListFragment.java
+++ src/it/wm/perdue/coupon/DetailCouponListFragment.java
@@ -3,10 +3,14 @@ package it.wm.perdue.coupon;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Parcelable;
+import android.text.Html;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,11 +22,18 @@ import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockListFragment;
 
+import it.wm.CachedAsyncImageView;
 import it.wm.HTTPAccess;
 import it.wm.perdue.LoggingHandler;
 import it.wm.perdue.R;
+import it.wm.perdue.Utils;
 import it.wm.perdue.businessLogic.Coupon;
 import it.wm.perdue.forms.BaseFormActivity;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class DetailCouponListFragment extends SherlockListFragment implements
         HTTPAccess.ResponseListener, OnClickListener {
@@ -35,14 +46,16 @@ public class DetailCouponListFragment extends SherlockListFragment implements
     private ProgressDialog                              progressDialog;
     
     // Gestione dello stato della lista:
-    protected CouponJSONAdapter<Coupon> adapter    = null;
+    protected DetailCouponAdapter<Coupon> adapter    = null;
     private Parcelable                                  listState  = null;
     
     //dati esercente
     protected String                                    eseId      = null;
     private Button                                      buyButton = null;
     private OnCouponActionListener                      listener = null;
-    
+    private CountDownTimer                              countDownTimer = null;
+    private long                                        millisTot = 0;
+    private boolean                                     isCouponOfTheDay = false;
     
     public interface OnCouponActionListener{
         public void onDidCheckout(Coupon c);
@@ -80,40 +93,28 @@ public class DetailCouponListFragment extends SherlockListFragment implements
         Bundle args = getArguments();
         //eseId = args.getString(ESE_ID);
         
+//      urlString = "http://www.cartaperdue.it/partner/v2.0/DettaglioEsercenteCompleto.php?id="
+//      + eseId;
+        
+        httpAccess = new HTTPAccess();
+        httpAccess.setResponseListener(this);
+        urlString = "http://www.cartaperdue.it/partner/android/coupon2.php?prov=" + "Roma";
+        httpAccess.startHTTPConnection(urlString, HTTPAccess.Method.GET,
+                null, TAG_NORMAL);
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Caricamento in corso...");
+        //progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(true); 
+
+        progressDialog.show();
+        onCreateAdapters();     
+        setListAdapter(adapter);
     }
     
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         Log.d("couponList","onActivityCreated");
-        
-        onCreateAdapters();
-        setListAdapter(adapter);
-
-        httpAccess = new HTTPAccess();
-        httpAccess.setResponseListener(this);
-//        urlString = "http://www.cartaperdue.it/partner/v2.0/DettaglioEsercenteCompleto.php?id="
-//                + eseId;
-        
-        String urlImageString = "http://www.cartaperdue.it/partner/v2.0/ImmagineEsercente.php?id="
-                + eseId;
-        
-        urlString = "http://www.cartaperdue.it/partner/android/coupon2.php?prov=" + "Roma";
-        httpAccess.startHTTPConnection(urlString, HTTPAccess.Method.GET,
-                null, TAG_NORMAL);
-                
-        progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setMessage("Caricamento in corso...");
-        //progressDialog.setIndeterminate(true);
-        progressDialog.setCancelable(true); 
-//        progressDialog.setOnDismissListener(new DialogInterface.OnCancelListener(){
-//           @Override
-//           public void onCancel(DialogInterface dialog)
-//           {
-//               **** cleanup.  Not needed if not cancelable ****
-//           }});
-
-        progressDialog.show();
     }
     
     @Override
@@ -126,7 +127,16 @@ public class DetailCouponListFragment extends SherlockListFragment implements
       TextView tv = (TextView) view.findViewById(R.id.summaryTextView);
       tv.setText("CIAOOOOO");
       
+      if(adapter.getObject() != null)
+          buyButton.setEnabled(true);
+      
         return view;
+    }
+    
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        httpAccess.setResponseListener(null);
     }
     
     @Override
@@ -153,18 +163,29 @@ public class DetailCouponListFragment extends SherlockListFragment implements
     
     public void onResume(){
         super.onResume();
-  
         Log.d("couponList","onResume");
+
+        millisTot = 0;
+        //su ios facevo così, qui non so se serve... 
+        if(countDownTimer != null){
+            countDownTimer.cancel();
+        }
+        
+        //forzo a ricaricare i dati del model per far ripartire il timer
+        adapter.notifyDataSetChanged();
     }
     
     public void onStop(){
         super.onStop();
         Log.d("couponList","onStop");
+        if(countDownTimer != null)
+            countDownTimer.cancel();
     }
     
+
     
     protected void onCreateAdapters() {
-        adapter = new CouponJSONAdapter<Coupon>(
+        adapter = new DetailCouponAdapter<Coupon>(
                 getActivity(),
                 R.layout.esercente_row, Coupon.class);
     }
@@ -190,7 +211,7 @@ public class DetailCouponListFragment extends SherlockListFragment implements
     public void onHTTPResponseReceived(String tag, String response) {
         // downloading--;
         
-        Log.d("couponList", "RISPOSTA = " + response);
+        //Log.d("couponList", "RISPOSTA = " + response);
         buyButton.setEnabled(true);
         
         if (tag.equals(TAG_NORMAL)) {
@@ -243,80 +264,125 @@ public class DetailCouponListFragment extends SherlockListFragment implements
         }
     }
     
-    /*
-    protected static class DettaglioEsercenteAdapter<T extends Esercente> extends
+    
+    protected class DetailCouponAdapter<T extends Coupon> extends
             CouponJSONAdapter<T> {
         
-        public DettaglioEsercenteAdapter(Context context, int resource, Class<T> clazz) {
+        public DetailCouponAdapter(Context context, int resource, Class<T> clazz) {
             super(context, resource, clazz);
           //  Log.d("UUU", "DETTAGLIO ESERCENTE ADAPTER --> count = "+sections.size());
         }
         
         public View getView(int position, View v, ViewGroup parent) {
             
-            v = super.getView(position, v, parent);
             
-            if (esercente != null) {
-                
-                TextView infoTextView = null;            
-
-                if (sections.get(position).equals("info")) {
-                    infoTextView = (TextView) v.findViewById(R.id.infoRow);
+            int layout = 0;
+            
+            switch(position){
+                case 0:
+                    //titolo
+                    layout = R.layout.coupon_title_row;
+                    break;
+                case 1:
+                    //dettaglio
+                    layout = R.layout.coupon_detail_row;
+                    break;
+                case 2:
+                    //altro
+                    layout = R.layout.coupon_options_row;
+                    break;
+            }
+            
+            if (v == null) {
+                v = inflater.inflate(layout, null);
+            }
+            if (coupon != null) {
+                if(position == 0){
+                    TextView title = (TextView) v.findViewById(R.id.coupon_title_row); 
+                    title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+                    title.setText(coupon.getTitoloBreve());
+                }
+                else if(position == 1){
+                    Log.d("couponList","coupon diverso null");
+                    TextView price = (TextView) v.findViewById(R.id.couponPrice);            
+                    TextView discount = (TextView) v.findViewById(R.id.couponDiscount);  
+                    TextView differencePrice = (TextView) v.findViewById(R.id.couponDifference);   
+                    //TextView timer = (TextView) v.findViewById(R.id.couponExipiry);  
+                    TextView normalPrice = (TextView) v.findViewById(R.id.couponNormalPrice);  
+                    CachedAsyncImageView image = (CachedAsyncImageView) v.findViewById(R.id.couponImage);
                     
-                    String giorniString = null;
-                    
-                    try {
-                        giorniString = (esercente.getGiorniString() != null ?
-                                "<b> Giorni validità </b>" + "<br />"
-                                + esercente.getGiorniString() + "<br />" : "");
-                    } catch (NullPointerException e) {
-                        Log.d(DEBUG_TAG, "eccezione in getView: " + e.getLocalizedMessage());
-                    }
-                    
-                    infoTextView.setText(Html.fromHtml((
-                            esercente.getGiornoChiusura() != null ? "<b> Giorno di chiusura</b>"
-                                    + "<br />" +
-                                    esercente.getGiornoChiusura() + "<br />" : "")
-                                    +
-                                    (giorniString != null ? giorniString : "")
-                                    + (esercente.getNoteVarie() != null ? "<b> Condizioni</b>" + "<br />"
-                                            + esercente.getNoteVarie() : "")));
-                    
+                    String urlImg = "http://www.cartaperdue.it/coupon/img_offerte/";
+                    image.loadImageFromURL(urlImg+coupon.getUrlImmagine());
+                    discount.setText(Html.fromHtml("<b> Sconto </b>" + "<br />"
+                            + coupon.getScontoPer()));
+                    differencePrice.setText(Html.fromHtml("<b> Risparmio </b>" + "<br />"
+                            + Utils.formatPrice(coupon.getSconto())+"€"));
+                    normalPrice.setText(Html.fromHtml("<b> Prezzo pieno </b>" + "<br />"
+                            + Utils.formatPrice(coupon.getValoreFacciale())+"€"));
+                    price.setText(Html.fromHtml("<b> Solo </b>" + "<br />"
+                            + Utils.formatPrice(coupon.getValoreAcquisto())+"€"));      
+                    //setTimer(timer);
                 }
             }
             return v;
         }
         
-        @Override
-        protected void checkFields() {
+        private void setTimer(TextView t){
+          //creo il timer appena la view torna visibile
+            Date now = new Date();
+            DateFormat formatter = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+            final TextView timer = t;
             
-            super.checkFields();
-            if (esercente.getGiorni() != null || esercente.getGiornoChiusura() != null ||
-                    esercente.getNoteVarie() != null) {
-                sections.add(0,"info");
+            try{
+                //Log.d("uffa","data scadenza coupon1 = "+coupon.getFineValidita().toString());
+                //Date scad = formatter.parse("Sat Mar 16 23:59:00 CET 2013");
+                //Log.d("uffa","data scadenza coupon2 = "+scad.toString());
+                //String scadT = formatter.format(coupon.getFineValidita().toString());
+                Log.d("uffa","data di scad ---> "+coupon.getFineValidita().toString());
+                Log.d("uffa", "data now --->"+formatter.format(now));
+                millisTot = coupon.getFineValidita().getTime() - now.getTime();
+                Log.d("Timer","millisecondti minutes = "+millisTot);
             }
+            catch(IllegalArgumentException e){
+                e.printStackTrace();
+            }
+            countDownTimer = new CountDownTimer(millisTot, 1000) {
+                int count = 0;
+                public void onTick(long millisUntilFinished) {
+                    //Log.d("COUNT","COUNT = "+(count++));
+                    //Log.d("Timer","millisUntilFinished = "+millisUntilFinished);
+                    countDown(millisUntilFinished, timer);
+                }
+
+                public void onFinish() {
+                    timer.setText(Html.fromHtml("<b>Scade tra:</b> offerta scaduta - 2"));
+                }
+             }.start();
         }
         
-        
-        // @Override
-        // public boolean isEnabled(int position) {
-        //
-        // if (sections.get(position).equals("info") ||
-        // sections.get(position).equals("info") ||
-        // sections.get(position).equals("map")
-        // || sections.get(position).equals("infoRisto")) {
-        // return false;
-        // }
-        // else
-        // return true;
-        // }
-        //
-        // @Override
-        // public boolean areAllItemsEnabled() {
-        // return true;
-        // }
-        //
-
+        private void countDown(long millisUntilFinished, TextView timer){
+            long days;
+            long minutes;
+            long seconds;
+            long hours;
+            long secondsLeft = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished);
+            
+            if (secondsLeft > 0) {
+                secondsLeft = secondsLeft - 1;
+                days= secondsLeft / (3600*24);
+                hours = (secondsLeft - (days * 24 * 3600)) / 3600;
+                minutes = (secondsLeft - ((hours * 3600) + (days * 24 * 3600))) / 60;
+                seconds = secondsLeft % 60;
+                
+                timer.setText(Html.fromHtml("<b>Scade tra:</b> "+days+"g "+hours+"h "+minutes+"m "+ seconds+"s"));
+            } 
+            else {
+                secondsLeft = 0;
+                timer.setText(Html.fromHtml("<b>Scade tra:</b> offerta scaduta - 1"));
+                //annullo il timer quando scade il countDown
+                countDownTimer.cancel();
+            }
+        }
     }
-    */
+    
 }
