05ea570c7b50d2f44f5cd6e1f3d3b0a851959a38
mario <eltazar@eltazar-kubuntu.(none)>
2013-03-11 16:33:27 +0100

    [MainActivity]: per ora chiama lista coupon

--- src/it/wm/perdue/MainActivity.java
+++ src/it/wm/perdue/MainActivity.java
@@ -18,7 +18,7 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
 import it.wm.perdue.contatti.InfoPerDueBaseActivity;
-import it.wm.perdue.coupon.ProvaCouponTimer;
+import it.wm.perdue.coupon.CouponsListFragment;
 import it.wm.perdue.doveusarla.DoveUsarlaFragment;
 import it.wm.perdue.forms.RichiediCartaFragment;
 
@@ -41,7 +41,7 @@ public class MainActivity extends SherlockFragmentActivity implements TabListene
         Utils.setContext(this.getApplicationContext());
         
         tabList = new ArrayList<TabDescriptor>();
-        tabList.add(new TabDescriptor(COUPON_TAB_TAG, ProvaCouponTimer.class, "Coupon"));
+        tabList.add(new TabDescriptor(COUPON_TAB_TAG, CouponsListFragment.class, "Coupon"));
         tabList.add(new TabDescriptor(DOVE_USARLA_TAB_TAG, DoveUsarlaFragment.class, "Dove Usarla"));
         tabList.add(new TabDescriptor(NEWS_TAB_TAG, NewsListFragment.class, "News"));
         tabList.add(new TabDescriptor(RICHIEDI_TAB_TAG, RichiediCartaFragment.class, "Richiedi"));
